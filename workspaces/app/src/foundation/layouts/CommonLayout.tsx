4f26b9663b2f5c222f3a77d63577a555297b6e66
Yuu Feuring <78978984+yuu8812@users.noreply.github.com>
2024-03-23 16:10:15 +0900

    still in progress

--- workspaces/app/src/foundation/layouts/CommonLayout.tsx
+++ workspaces/app/src/foundation/layouts/CommonLayout.tsx
@@ -6,7 +6,7 @@ import { Footer } from '../components/Footer';
 import { Space } from '../styles/variables';
 
 const _Content = styled.div`
-  height: 100%;
+  min-height: 100vh;
   padding: 0 ${Space * 2}px;
 `;
 
